e2bae8bd0d3f8659efd7c6dc1cd6b9a856dfae36
Anmol <a.mahajan110104@gmail.com>
2023-07-22 03:46:42 +0530

    changes

--- server/backend/server.js
+++ server/backend/server.js
@@ -39,7 +39,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeOut: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
   },
 });
 
